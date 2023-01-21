0101dd8b13f70219db5ec3b5daea4a7491f1f688
lsweyrauch <123258498+lsweyrauch@users.noreply.github.com>
2023-01-21 10:32:41 -0600

    Homework 1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HeloWorld;
+class Program
+{
+    static void Main(string[] args)
+    {
+        float x = 2.5f;
+        float y = 3.3f;
+        Console.WriteLine((4*x*x)+(3*y));
+    }
+}
